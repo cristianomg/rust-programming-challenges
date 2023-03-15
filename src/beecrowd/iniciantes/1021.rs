4380e86ba6da20af5f6419f9d6a1d8d9e431037f
Cristiano Macedo <cristianomacedo@MacBook-Air-de-Cristiano.local>
2023-03-15 20:25:46 -0300

    1021

--- src/beecrowd/iniciantes/1021.rs
+++ src/beecrowd/iniciantes/1021.rs
@@ -2,15 +2,15 @@
 use std::io;
 use std::collections::BTreeMap;
 
-const NOTES: [f64; 6] = [100_f64, 50_f64, 20_f64, 10_f64, 5_f64, 2_f64];
+const NOTES: [f32; 6] = [100_f32, 50_f32, 20_f32, 10_f32, 5_f32, 2_f32];
 
-const COINS: [f64; 6] = [1.0_f64, 0.50_f64, 0.25_f64, 0.10_f64, 0.05_f64, 0.01_f64];
+const COINS: [f32; 6] = [1.0_f32, 0.50_f32, 0.25_f32, 0.10_f32, 0.05_f32, 0.01_f32];
 
 fn main (){
     let mut imput = String::new();
     io::stdin().read_line(&mut imput).unwrap();
 
-    let n: f64 = imput.trim().parse().unwrap();
+    let n: f32 = imput.trim().parse().unwrap();
 
     let mut result_notes: BTreeMap<i32, i32> = BTreeMap::new();
     let mut result_coins: BTreeMap<usize, i32> = BTreeMap::new();
@@ -18,14 +18,14 @@ fn main (){
     let mut acc = n;
     
     acc = decompose_notes(acc, &mut result_notes);
-    _ = decompose_coins(acc, &mut result_coins);
+    acc = decompose_coins(acc, &mut result_coins);
     
     show_notes(result_notes);
     show_coins(result_coins, COINS);
 }
 
 
-fn decompose_notes(mut acc: f64, result_notes: &mut BTreeMap<i32, i32>) -> f64{
+fn decompose_notes(mut acc: f32, result_notes: &mut BTreeMap<i32, i32>) -> f32{
     for note in NOTES.iter() {
         let (value, quantity) = decompose(acc, *note);
         
@@ -36,10 +36,10 @@ fn decompose_notes(mut acc: f64, result_notes: &mut BTreeMap<i32, i32>) -> f64{
     return acc;
 }
 
-fn decompose_coins(mut acc: f64, result_coins: &mut BTreeMap<usize,i32>) -> f64{
+fn decompose_coins(mut acc: f32, result_coins: &mut BTreeMap<usize,i32>) -> f32{
 
     for coin_index in 0..COINS.len() {
-        let coin: f64 = COINS[coin_index];
+        let coin: f32 = COINS[coin_index];
         let (value, quantity) = decompose(acc, coin);
         result_coins.insert(coin_index, quantity);
 
@@ -49,7 +49,7 @@ fn decompose_coins(mut acc: f64, result_coins: &mut BTreeMap<usize,i32>) -> f64{
     return acc;
 }
 
-fn decompose(mut value: f64, note: f64) -> (f64, i32) {
+fn decompose(mut value: f32, note: f32) -> (f32, i32) {
     let mut quantity = 0;
     loop {
         if value < note {
@@ -68,7 +68,7 @@ fn show_notes(result_notes: BTreeMap<i32, i32>){
         println!("{} nota(s) de R$ {},00", quantity, note);
     }
 }
-fn show_coins(result_coins: BTreeMap<usize, i32>, coins_var: [f64; 6]){
+fn show_coins(result_coins: BTreeMap<usize, i32>, coins_var: [f32; 6]){
     println!("MOEDAS:");
     for (coin_index, quantity) in result_coins.iter(){
         println!("{} moeda(s) de R$ {:.2}", quantity, coins_var[*coin_index]);
